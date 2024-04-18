da2f5d35cb79953ad3659df44e1e50e009515ffb
Plaban <turjoyplaban09@gmail.com>
2024-04-18 09:20:28 +0600

    Add function implemented

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et1;
+    EditText et2;
+    TextView tv;
+
+    int val1,val2;
+
+    Button bt1,bt2,bt3,bt4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.editTextTextPersonName);
+        et2 = findViewById(R.id.editTextTextPersonName2);
+        tv = findViewById(R.id.textView2);
+
+        bt1 = findViewById(R.id.button5);
+        bt2 = findViewById(R.id.button6);
+        bt3 = findViewById(R.id.button7);
+        bt4 = findViewById(R.id.button8);
+        addFunction();
     }
+
+    private void addFunction() {
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                val1 = Integer.parseInt(et1.getText().toString());
+                val2 = Integer.parseInt(et2.getText().toString());
+                tv.setText("Result is : "+(val1+val2));
+            }
+        });
+    }
+
+    private void subFunction() {
+
+    }
+
+    private void mulFunction() {
+
+    }
+
+    private void divFunction() {
+
+    }
+
+
 }
