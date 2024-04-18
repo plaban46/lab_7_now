5d0b7be21ead93162a0d3feef219c1cdb6e9d83f
Plaban <turjoyplaban09@gmail.com>
2024-04-18 09:26:07 +0600

    Rules

---
+++ app/src/androidTest/java/com/example/test/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+ 
+
+
+}
